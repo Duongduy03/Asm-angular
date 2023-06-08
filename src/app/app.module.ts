06a3cb1bc61a252b9ac78628dbffd18e9a8f8dbe
luongph28185 <luongttph28185@fpt.edu.vn>
2023-06-08 08:00:41 +0700

    luong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
@@ -49,7 +49,7 @@ import { BlogComponent } from './pages/client/blog/blog.component';
     ContactComponent,
     BlogComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/client/signin/signin.component.ts
+++ src/app/pages/client/signin/signin.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent {
-
+  formSignin = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+  onHandleSubmit() {
+    if (this.formSignin.valid) {
+      this.auth.signin(this.formSignin.value).subscribe((data) => {
+        localStorage.setItem('credential', JSON.stringify(data));
+      });
+    }
+  }
 }
